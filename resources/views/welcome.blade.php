180c5b4d39a2495bcedf95adc0efe32397150f83
millooo94 <camillomusmeciunict@gmail.com>
2023-02-15 17:05:27 +0100

    add Dario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Ludovico</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laravel-news.com">Dario</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
