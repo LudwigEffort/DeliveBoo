6447d716e6c295a0b521211ebdf08a28cd75afa0
millooo94 <camillomusmeciunict@gmail.com>
2023-02-15 16:54:09 +0100

    add camillo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CAMILLO
                 </div>
 
                 <div class="links">
