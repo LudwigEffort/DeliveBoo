983e3d7ac0db3682b210ffccd819642a6d52557e
LudwigEffort <dr.feynman65@gmail.com>
2023-02-16 12:43:02 +0100

    create develop branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CAMILLO
+                    Queto è il Develop
                 </div>
 
                 <div class="links">
