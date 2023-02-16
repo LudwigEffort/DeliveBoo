5aef7108ff33dad176ee0bee66ef4159d8d65310
LudwigEffort <dr.feynman65@gmail.com>
2023-02-16 17:41:00 +0100

    add dishes table

---
+++ database/migrations/2023_02_16_163515_create_dishes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 100)->unique();
+            $table->string('name', 50);
+            $table->text('description');
+            $table->mediumInteger('price')->unsigned();
+            $table->tinyInteger('available')->unsigned();
+            $table->tinyInteger('is_visible')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
