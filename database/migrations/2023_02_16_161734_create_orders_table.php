8da2d1cdf1e42f7129ea8a5cef83435abb05cf84
AleAlle62 <basketallegrini@gmail.com>
2023-02-16 17:19:58 +0100

    add table orders

---
+++ database/migrations/2023_02_16_161734_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('f_name', 50);
+            $table->string('l_name', 50);
+            $table->string('email')->unique();
+            $table->dateTime('order_date');
+            $table->dateTime('pickup_date');
+            $table->dateTime('payment_date');
+            $table->mediumInteger('amount')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
