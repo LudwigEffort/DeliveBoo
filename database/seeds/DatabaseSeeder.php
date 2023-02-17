dd99d844d6d1a9b0b07080ed5532994d230b6a48
millooo94 <camillomusmeciunict@gmail.com>
2023-02-17 16:06:39 +0100

    add dish seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(DishSeeder::class);
 
 
     }
---
+++ database/seeds/DishSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Dish;
+use Illuminate\Database\Seeder;
+
+class DishSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dishes = [
+            'Pasta alla carbonara',
+            'Pizza',
+            'Pasta allo scoglio',
+            'Kebab',
+            'user_id' =>
+            'Pulled pork',
+            'Hamburger',
+        ];
+
+        foreach ($dishes as $dish) {
+            Dish::create([
+                'user_id'       => rand(1, 3), //da sistemare con faker
+                'slug'          => Dish::getSlug($dish),
+                'name'          => $dish,
+                'description'   => "lorem ipsum blablablablablabla",
+                'price'         => 1000,
+                'available'     => rand(0, 1),
+                'is_visible'    => rand(0, 1), //da definire
+            ]);
+        }
+    }
+
+}
