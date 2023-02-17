9914b837dc1ef6bf0dfa0e256f335d89aba4fc7a
LudwigEffort <dr.feynman65@gmail.com>
2023-02-17 10:52:46 +0100

    add realtion many to many between dish order

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function dishes() {
+        return $this->belongsToMany('App\Dish');
+    }
 }
---
+++ database/migrations/2023_02_17_094936_create_dish_order_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dish_order', function (Blueprint $table) {
+
+            $table->foreignId('dish_id')->constrained();
+
+            $table->foreignId('order_id')->constrained();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dish_order');
+    }
+}
