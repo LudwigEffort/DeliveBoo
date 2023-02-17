70da1e710ef1d57aea1e73146d206df9fe6a75b4
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-02-17 11:01:24 +0100

    tolti commenti in dish order

--- database/migrations/2023_02_17_094936_create_dish_order_table.php
+++ database/migrations/2023_02_17_094936_create_dish_order_table.php
@@ -6,27 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateDishOrderTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('dish_order', function (Blueprint $table) {
-
             $table->foreignId('dish_id')->constrained();
-
             $table->foreignId('order_id')->constrained();
 
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('dish_order');
