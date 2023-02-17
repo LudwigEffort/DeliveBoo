a06269a35f3355e79e05ae9ff126b7a11a1e48ac
LudwigEffort <dr.feynman65@gmail.com>
2023-02-17 10:55:39 +0100

    done realtion many to many between dish order

--- app/Dish.php
+++ app/Dish.php
@@ -11,4 +11,8 @@ class Dish extends Model
     public function user() {
         return $this->belongsTo('App\User');
     }
+
+    public function orders() {
+        return $this->belongsToMany('App\Order');
+    }
 }
