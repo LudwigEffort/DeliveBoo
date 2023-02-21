c04dd0816cde9c40ccb74883a8f9d5cf0e989c57
LudwigEffort <dr.feynman65@gmail.com>
2023-02-21 18:58:21 +0100

    fix issue with uploader img of user

--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'vat_number', 'email', 'password'
+        'name', 'vat_number', 'email', 'password' , 'uploaded_img'
     ];
 
     protected $attributes = [
