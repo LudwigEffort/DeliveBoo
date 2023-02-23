1629214813689eed53a14dc3ee763c6461d9caae
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-02-23 18:12:17 +0100

    da fixare e ricontrollare singoli ristoranti e i suoi dishes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users',    'Api\UserController@index')->name('users.index');
 Route::get('/users/{user:slug}', 'Api\UserController@show')->name('users.show');
 
-//API DISHES
-Route::get('/{users:slug}/dishes', 'Api\DishController@index')->name('dishes.index');
-Route::get('/{users:slug}/dishes/{dish:slug}', 'Api\DishController@show')->name('dishes.show');
+//API DISHES DEI SINGOLI RESTAURANTS (USERS)
+Route::get('/users/{dish:slug}', 'Api\DishController@index')->name('dishes.index');
+Route::get('/users/{user:slug}/{dish:slug}', 'Api\DishController@show')->name('dishes.show');
