1a0bcec98ef3a343fbb741d9fbc5ca1cd6a0cd36
LudwigEffort <dr.feynman65@gmail.com>
2023-02-25 11:30:19 +0100

    client token gateway work and change output in json

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -10,7 +10,16 @@ class PaymentController extends Controller
 {
     public function generate(Request $request, Gateway $gateway){
 
-        dd($gateway->clientToken());
+        //dd($gateway->clientToken()->generate());
+
+        $token = $gateway->clientToken()->generate();
+
+        $data = [
+            'success'   => true,
+            'token'     => $token
+        ];
+
+        return response()->json($data, 200);
 
         return  'generate';
     }
