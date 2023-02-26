81369c3f871f6623fd43b16f490e07e9901df2f6
LudwigEffort <dr.feynman65@gmail.com>
2023-02-26 18:45:18 +0100

    add AmountConvert trait in Order model

--- app/Order.php
+++ app/Order.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use App\Traits\AmountConvert;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    use AmountConvert;
+
     public function dishes() {
         return $this->belongsToMany('App\Dish');
     }
