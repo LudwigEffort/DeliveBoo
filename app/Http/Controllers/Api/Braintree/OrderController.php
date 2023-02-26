54100eda909e933cc49ef3bec56ac7ca72f07f72
LudwigEffort <dr.feynman65@gmail.com>
2023-02-26 18:26:57 +0100

    first version of OrderController in api

--- app/Http/Controllers/Api/Braintree/OrderController.php
+++ app/Http/Controllers/Api/Braintree/OrderController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api\Braintree;
 
 use App\Http\Controllers\Controller;
+use App\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index(Request $request){
+
+        $orders = Order::all();
+
+        return response()->json($orders, 200);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::get('/users/{user:slug}', 'Api\UserController@show')->name('users.show');
 //Api payment by Braintree
 Route::get('/payment/generate', 'Api\Braintree\PaymentController@generate');
 Route::post('/payment/make/payment', 'Api\Braintree\PaymentController@makePayment');
+
+// API to generate Orders from db
+Route::get('/orders', 'Api\Braintree\OrderController@index');
