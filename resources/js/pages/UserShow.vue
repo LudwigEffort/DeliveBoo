cb4009fe142edd7f26caaa6461ee8ae3fd52fc1c
aCute5 <costantini95dario@gmail.com>
2023-03-03 13:18:51 +0100

    counter dish added

--- resources/js/pages/UserShow.vue
+++ resources/js/pages/UserShow.vue
@@ -1,70 +1,77 @@
 <template>
     <div class="container mt-5">
-      <div v-if="user">
-        <h1>{{ user.name }}</h1>
-        <h1>{{ user.opening_time }}</h1>
-        <div v-for="dish in dishes" :key="dish.id">
-          <h3>{{ dish.name }}</h3>
-          <p>{{ dish.description }}</p>
-          <p>{{ dish.price }}$</p>
-          <div>
-            <button class="text-white" @click="addToCart(dish)">aggiungi</button>
-          </div>
+        <div v-if="user">
+            <h1>{{ user.name }}</h1>
+            <h1>{{ user.opening_time }}</h1>
+            <div v-for="dish in dishes" :key="dish.id">
+                <h3>{{ dish.name }}</h3>
+                <p>{{ dish.description }}</p>
+                <p>{{ dish.price }}$</p>
+                <div>
+                    <button class="text-white" @click="addToCart(dish)">
+                        aggiungi
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
+</template>
 
-  <script>
-  import axios from 'axios';
+<script>
+import axios from "axios";
 
-  export default {
-    name: 'UserShow',
+export default {
+    name: "UserShow",
     data() {
-      return {
-        user: {},
-        cart: [],
-        dishes: [],
-      };
+        return {
+            user: {},
+            cart: [],
+            dishes: [],
+        };
     },
     mounted() {
-      axios.get(`/api/users/${this.$route.params.slug}`)
-        .then(response => {
-          this.dishes = response.data.dishes;
-          this.user = response.data.user;
-        })
-        .catch(error => {
-          console.error(error);
-        });
+        axios
+            .get(`/api/users/${this.$route.params.slug}`)
+            .then((response) => {
+                this.dishes = response.data.dishes;
+                this.user = response.data.user;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     },
     methods: {
-      addToCart(dish) {
-        this.$forceUpdate();
-        this.cart.push({
-      id: dish.id,
-      name: dish.name,
-      price: dish.price,
-      quantity: 1,
-    })
-        this.$emit('cart-updated', this.cart);
-        console.log(this.cart);
-      },
-//   addToCart(dish) {
-//   const orderedDishIndex = this.cart.findIndex(item => item.id === dish.id);
-//   if (orderedDishIndex > -1) {
-//     this.cart[orderedDishIndex].quantity++;
-//   } else {
-//     this.cart.push({
-//       id: dish.id,
-//       name: dish.name,
-//       price: dish.price,
-//       quantity: 1,
-//     });
-//   }
-//   this.$emit('cart-updated', this.cart);
+        addToCart(dish) {
+            const orderedDish = this.cart.find((item) => item.id === dish.id);
+            if (orderedDish) {
+                orderedDish.quantity++;
+            } else {
+                this.cart.push({
+                    id: dish.id,
+                    name: dish.name,
+                    price: dish.price,
+                    quantity: 1,
+                });
+            }
+            this.$emit("cart-updated", this.cart);
+            console.log(this.cart);
+            //   addToCart(dish) {
+            //   const orderedDishIndex = this.cart.findIndex(item => item.id === dish.id);
+            //   if (orderedDishIndex > -1) {
+            //     this.cart[orderedDishIndex].quantity++;
+            //   } else {
+            //     this.cart.push({
+            //       id: dish.id,
+            //       name: dish.name,
+            //       price: dish.price,
+            //       quantity: 1,
+            //     });
+            //   }
+            //   this.$emit('cart-updated', this.cart);
+        },
     },
-  }
+};
+</script>
 
-  </script>
-
-  <style scoped lang="scss"></style>
+<style scoped lang="scss"></style>
+this.$forceUpdate();
