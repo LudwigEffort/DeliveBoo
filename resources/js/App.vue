9a64ce886c56675d2a5c3eba9005ae889e338c3a
aCute5 <costantini95dario@gmail.com>
2023-03-03 21:25:29 +0100

    starting layout

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,16 +1,13 @@
 <template>
-    <div>
-        <HeaderPage
-        @search-changed="updateSearch"
-        :cart="cart">
-        </HeaderPage>
+    <div class="container-fluid">
+        <HeaderPage @search-changed="updateSearch" :cart="cart"> </HeaderPage>
         <router-view
-            :cart = "cart"
-            :search = "search"
-            :users = "users"
-            :category = "category"
-            :categories = "categories"
-            @total_price = "finalPrice"
+            :cart="cart"
+            :search="search"
+            :users="users"
+            :category="category"
+            :categories="categories"
+            @total_price="finalPrice"
             @category-changed="updateCategory"
             @cart-updated="updateCart"
         >
@@ -19,37 +16,35 @@
 </template>
 
 <script>
-import HeaderPage from './components/HeaderPage.vue';
+import HeaderPage from "./components/HeaderPage.vue";
 
 export default {
-    name: 'App',
+    name: "App",
     components: {
         HeaderPage,
     },
     data() {
         return {
             users: [],
-            search: '',
-            category: '',
+            search: "",
+            category: "",
             categories: [],
             cart: [],
-            finalAmount: '',
-        }
+            finalAmount: "",
+        };
     },
 
     mounted() {
-     this.getUsers();
-
+        this.getUsers();
     },
 
     methods: {
+        updateSearch(search) {
+            this.search = search;
+            this.getUsers();
+        },
 
-    updateSearch(search) {
-      this.search = search;
-      this.getUsers();
-    },
-
-    getUsers() {
+        getUsers() {
             let params = {};
 
             if (this.search) {
@@ -60,43 +55,42 @@ export default {
 
             if (this.category) {
                 params.category = this.category;
-                this.category = false
+                this.category = false;
             }
 
-            axios.get('/api/users', {
-                params: params,
-            })
-                .then(response => {
+            axios
+                .get("/api/users", {
+                    params: params,
+                })
+                .then((response) => {
                     this.users = response.data.results;
                     this.categories = response.data.categories;
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error(error);
                 });
         },
         updateCategory(category) {
-        this.category = category;
-        this.getUsers();
+            this.category = category;
+            this.getUsers();
         },
         updateCart(cart) {
             this.cart = cart;
         },
         finalPrice(finalAmount) {
             this.finalAmount = finalAmount;
-        }
-  },
-}
-
+        },
+    },
+};
 </script>
 
 <style lang="scss">
-@import '~bootstrap/scss/bootstrap';
+@import "~bootstrap/scss/bootstrap";
 
 .card {
- border-radius: 30px;
- background: white;
- box-shadow: 15px 15px 30px rgb(25, 25, 25),
-             -15px -15px 30px rgb(60, 60, 60);
+    border-radius: 30px;
+    background: white;
+    box-shadow: 15px 15px 30px rgb(25, 25, 25), -15px -15px 30px rgb(60, 60, 60);
 }
 
 a {
@@ -108,41 +102,36 @@ a {
 // }
 
 button {
-  /* Variables */
- --button_radius: 0.75em;
- --button_color: #e8e8e8;
- --button_outline_color: #000000;
- font-size: 17px;
- font-weight: bold;
- border: none;
- border-radius: var(--button_radius);
- background: var(--button_outline_color);
+    /* Variables */
+    --button_radius: 0.75em;
+    --button_color: #e8e8e8;
+    --button_outline_color: #000000;
+    font-size: 17px;
+    font-weight: bold;
+    border: none;
+    border-radius: var(--button_radius);
+    background: var(--button_outline_color);
 }
 
 .button_top {
- display: block;
- box-sizing: border-box;
- border: 2px solid var(--button_outline_color);
- border-radius: var(--button_radius);
- padding: 0.75em 1.5em;
- background: var(--button_color);
- color: var(--button_outline_color);
- transform: translateY(-0.2em);
- transition: transform 0.1s ease;
+    display: block;
+    box-sizing: border-box;
+    border: 2px solid var(--button_outline_color);
+    border-radius: var(--button_radius);
+    padding: 0.75em 1.5em;
+    background: var(--button_color);
+    color: var(--button_outline_color);
+    transform: translateY(-0.2em);
+    transition: transform 0.1s ease;
 }
 
 button:hover .button_top {
-  /* Pull the button upwards when hovered */
- transform: translateY(-0.33em);
+    /* Pull the button upwards when hovered */
+    transform: translateY(-0.33em);
 }
 
 button:active .button_top {
-  /* Push the button downwards when pressed */
- transform: translateY(0);
+    /* Push the button downwards when pressed */
+    transform: translateY(0);
 }
-
 </style>
-
-
-
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,68 +8,21 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        {{-- Style --}}
 
         <script src="{{ asset('js/front.js') }}" defer></script>
+        <link rel="stylesheet" href="{{ asset('sass/back.scss')}}">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <style lang="scss" scoped>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            /* .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            } */
-
-            .position-ref {
-                position: relative;
-            }
-
-            /* .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            } */
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+
             @if (Route::has('login'))
-            {{-- TOGLIERE QUESTA CLASSE TOP-RIGHT --}}
-                <div class="top-right links">
+                <div class="row">
+
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                         <form method="POST" action="/logout">
@@ -77,15 +30,23 @@
                             <input type="submit" value="Logout">
                         </form>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                    <div class="logbuttons">
+                        <div class="col-6">
+                            <button class="bg-light">
+                                <a class="logbuttons" href="{{ route('login') }}"> Log in  Dining</a>
+                            </button>
+                        </div>
+                        <button class="bg-light">
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Apri il tuo ristorante  </a>
+                        </button>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
                         @endif
-                        <div id="root"></div>
-                    @endauth
-                </div>
-            @endif
-        </div>
+                        @endauth
+                        @endif
+                    </div>
+                    </div>
+
+            <div id="root" class="container-fluid"></div>
     </body>
 </html>
