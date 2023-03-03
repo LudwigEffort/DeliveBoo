1938ffca58b4f385908e5ea6a34f216d3269e37f
LudwigEffort <dr.feynman65@gmail.com>
2023-03-03 22:08:37 +0100

    refactor category seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -13,26 +13,66 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
+
         $categories = [
-            'Giapponese sushi',
-            'Cinese',
-            'Italiano',
-            'Arabo',
-            'Francese',
-            'Cucina internazionale',
-            'Koreano',
-            'Marocchina',
+            [
+                'name'  => 'Italiano',
+                'slug'  =>  Category::getSlug('Italiano'),
+                'description' => 'Cucina tadizionale italiana',
+            ],
+            [
+                'name'  => 'Giapponese',
+                'slug'  =>  Category::getSlug('Giapponese'),
+                'description' => 'Cucina tadizionale giapponese',
+            ],
+            [
+                'name'  => 'Americano',
+                'slug'  =>  Category::getSlug('Americano'),
+                'description' => 'Cucina americana',
+            ],
+            [
+                'name'  => 'Pizza',
+                'slug'  =>  Category::getSlug('Pizza'),
+                'description' => 'Pizzeria',
+            ],
+            [
+                'name'  => 'Sushi',
+                'slug'  =>  Category::getSlug('Sushi'),
+                'description' => 'Sushi',
+            ],
+            [
+                'name'  => 'Ristorante',
+                'slug'  =>  Category::getSlug('Ristorante'),
+                'description' => 'Ristorante',
+            ],
+            [
+                'name'  => 'Hamburger',
+                'slug'  =>  Category::getSlug('Hamburger'),
+                'description' => 'Hamburger',
+            ],
+            [
+                'name'  => 'Steakhouse',
+                'slug'  =>  Category::getSlug('Steakhouse'),
+                'description' => 'Steakhouse',
+            ],
+            [
+                'name'  => 'Kebab',
+                'slug'  =>  Category::getSlug('Kebab'),
+                'description' => 'Kebab',
+            ],
+            [
+                'name'  => 'Pasticceria',
+                'slug'  =>  Category::getSlug('Pasticceria'),
+                'description' => 'Pasticceria',
+            ],
         ];
 
         foreach ($categories as $category) {
-            $category = Category::create([
-                'slug' => Category::getSlug($category),
-                'name' => $category,
-                'description' => "lorem ipsum blablablablablabla",
-            ]);
+            $category = Category::create($category);
+        }
+
             $num_users = rand(1, 3);
             $user_ids = User::where('id', '<>', $category->id)->inRandomOrder()->take($num_users)->pluck('id')->toArray();
             $category->users()->sync($user_ids);
-        }
-}
+    }
 }
