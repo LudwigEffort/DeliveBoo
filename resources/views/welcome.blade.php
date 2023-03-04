aef96c871a22542c4a6389630577165ef3fba6cc
aCute5 <costantini95dario@gmail.com>
2023-03-04 18:39:49 +0100

    buttons fixed adding style on welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,20 +18,47 @@
 
         <!-- Styles -->
         <style lang="scss">
+            button {
+ appearance: none;
+ background-color: transparent;
+ border: 0.125em solid #1A1A1A;
+ border-radius: 0.9375em;
+ box-sizing: border-box;
+ color: #3B3B3B;
+ cursor: pointer;
+ display: inline-block;
+ font-family: Roobert,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+ font-size: 16px;
+ font-weight: 600;
+ line-height: normal;
+ margin: 0;
+ outline: none;
+ padding: 1em 2.3em;
+ text-align: center;
+ text-decoration: none;
+ transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
+ user-select: none;
+ -webkit-user-select: none;
+ touch-action: manipulation;
+ will-change: transform;
+}
 
-            .logbuttons {
+button:disabled {
+ pointer-events: none;
+}
 
-            }
-            .logbuttons button {
-                font-weight: bold;
-                color: black;
-                border: none;
-                background-color: #3653F8;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                font-variant: small-caps
-            }
+button:hover {
+ color: white
+ background-color: #1A1A1A;
+ transform: translateY(-2px);
+}
+
+button:active {
+ box-shadow: none;
+ transform: translateY(0);
+}
+
+
 
             a {
                 text-decoration: none !important;
@@ -44,12 +71,14 @@
                 position: absolute;
                 z-index: 3;
                 right: 20rem;
+                display: flex;
+
             }
 
         </style>
     </head>
     <body>
-          <div class="backend">
+          <div class="backend row">
             @if (Route::has('login'))
             <div class="row">
 
@@ -60,12 +89,12 @@
                         <input type="submit" value="Logout">
                     </form>
                 @else
-                <div class="logbuttons py-2 ps-3">
+                <div class=" col-12 logbuttons py-2 ps-3">
                     <div class="col-6">
                         <button class="mb-3">
                             <a class="logbuttons" href="{{ route('login') }}"> Log in  Dining</a>
                         </button>
-                    </div>
+                    </div class="col-6">
                     <button class="">
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Apri il tuo ristorante  </a>
