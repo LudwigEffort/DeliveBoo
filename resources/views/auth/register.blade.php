b23be5939c9511c0c99c7711b5ce1c26d68e3240
AleAlle62 <basketallegrini@gmail.com>
2023-03-04 12:13:22 +0100

    register ok

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,23 +5,23 @@
     <div class="row justify-content-center" style="margin-top:0px">
         <div class="col-md-5">
             {{-- card generale  --}}
-            <div class="card" style=" border: 1px solid #2001e5">
+            <div class="card" style="border:1px solid blue">
                 {{-- header card --}}
-                <div class="card-header"
-                style="border-bottom: 1px solid #2001e5;
-                background-color:blue;
-                color: white;
-                display:flex;
-                justify-content:center;
-                ">{{ __('REGISTER') }}</div>
+                <div class="card-header" style="color:blue;">{{ ('REGISTER') }}</div>
 
                 {{-- resto della card  --}}
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                         @csrf
 
+
+
+
+                        {{-- PROVA  --}}
+
+
                         {{-- nome --}}
-                        <div class="form-group row">
+                        <div class="inputBox">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('NOME') }}</label>
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -32,6 +32,11 @@
                                 @enderror
                             </div>
                         </div>
+
+
+
+
+
 
                         {{-- categoria  --}}
                         <div class="mb-3" style="margin-top: 20px;">
@@ -54,7 +59,7 @@
 
 
                         {{-- immagine  --}}
-                        <div class="form-group row">
+                        <div  class="inputBox">
                             <label for="uploaded_img" class="form-label" >CARICA IMMAGINE</label>
                             <input class="form-control @error('uploaded_img') is-invalid @enderror"
                             type="file" id="uploaded_img" name="uploaded_img">
@@ -71,7 +76,7 @@
 
 
                         {{-- email  --}}
-                        <div class="form-group row " style="margin-top: 20px;">
+                        <div  class="inputBox">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('EMAIL') }}</label>
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -83,7 +88,8 @@
                             </div>
 
                             {{-- vat number  --}}
-                                <label style="margin-top: 30px;" for="vat_number" class="col-md-4 col-form-label text-md-right">{{ __('PARTITA IVA') }}</label>
+                            <div  class="inputBox">
+                                <label  for="vat_number" class="col-md-4 col-form-label text-md-right">{{ __('PARTITA IVA') }}</label>
                                 <div class="col-md-6" style="margin-top: 30px;">
                                     <input id="vat_number" type="text" class="form-control @error('vat_number') is-invalid @enderror" name="vat_number" value="{{ old('vat_number') }}">
                                     @error('vat_number')
@@ -96,7 +102,7 @@
 
 
                             {{-- password --}}
-                        <div class="form-group row" style="margin-top: 30px;">
+                        <div class="inputBox">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('PASSWORD') }}</label>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -110,7 +116,7 @@
 
 
                         {{-- conferma  --}}
-                        <div class="form-group row" style="margin-top: 30px;">
+                        <div  class="inputBox">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('CONFERMA PASSWORD') }}</label>
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -118,12 +124,8 @@
                         </div>
 
                         {{-- BOTTONE  --}}
-                        <div class="form-group row mb-0" >
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style=" color: white; background-color:#2001e5;">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                        <div>
+                            <button class="enter">submit</button>
                         </div>
                     </form>
                 </div>
@@ -135,43 +137,84 @@
 
 
 
+<style lang="scss" scoped>
 
-{{-- nuovo form  --}}
+form {
+    color: blue;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
-
-
-<div class="container">
-    <div class="card">
-        {{-- form  --}}
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            {{-- email  --}}
-            <a class="login">Log in</a>
-            <div class="inputBox">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus >
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror>
-                <span class="user">Username</span>
-            </div>
-
+    gap: 30px;
+}
+.card-header {
+    background-color: white;
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    display: block;
+    font-weight: bold;
+    font-size: x-large;
+    text-decoration: none;
+}
 
 
-            {{-- password  --}}
-            <div class="inputBox">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror>
-                <span>Password</span>
-            </div>
-            <button class="enter">Enter</button>
-        </form>
-    </div>
-</div>
+
+/* prova  */
+
+
+.inputBox {
+    position: relative;
+    width: 200px;
+}
+
+.inputBox input {
+    width: 30vw;
+    padding: 10px;
+    outline: none;
+    border: none;
+    color: blue;
+    font-size: 1em;
+    background: transparent;
+    transition: 0.1s;
+    border: 2px solid blue;
+}
+
+
+.inputBox label {
+    width: 200px;
+}
+
+
+.enter {
+    margin-top: 20px;
+    color: blue;
+    height: 45px;
+    width: 100%;
+    border-radius: 5px;
+    border: 2px solid blue;
+    cursor: pointer;
+    background-color: transparent;
+    transition: 0.5s;
+    text-transform: uppercase;
+    font-size: 10px;
+    letter-spacing: 2px;
+    margin-bottom: 1em;
+}
+
+.enter:hover {
+    background-color: blue;
+    color: white;
+}
+
+
+
+
+
+
+</style>
+
+
+
 
 
