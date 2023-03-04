fd5e1d8b574198a2ba289a64d79c25b73d182e7a
LudwigEffort <dr.feynman65@gmail.com>
2023-03-04 18:20:47 +0100

    home.blade.php almost done

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,30 +3,39 @@
 @section('content')
 <div class="container">
         @if ($user->id <= 5)
-            <img src="{{ asset($user->uploaded_img) }}" alt="{{$user->id}}">
+            <img class="img-fluid" src="{{ asset($user->uploaded_img) }}" alt="{{$user->id}}">
         @else
-            <img src="{{ asset('storage/' . $user->uploaded_img) }}" alt="{{$user->id}}">
+            <img class="img-fluid" src="{{ asset('storage/' . $user->uploaded_img) }}" alt="{{$user->id}}">
         @endif
-        <h1>{{$user->name}}</h1>
-        <p>{{$user->description}}</p>
-        <div>{{$user->email}}</div>
-        <div>{{$user->vat_number}}</div>
-        @foreach ($user->categories as $category)
-        <div>{{$category->name}}</div>
-        @endforeach
-        <div>{{$user->opening_time}}</div>
-        <div>{{$user->closing_time}}</div>
+        <div class="row">
+            <h1>{{$user->name}}</h1>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col">
+                <h4>{{$user->email}}</h4>
+                <h4>{{$user->opening_time}}</h4>
+                <h4>{{$user->closing_time}}</h4>
+            </div>
+            <div class="col">
+                <ul>
+                    @foreach ($user->categories as $category)
+                    <ul class="btn btn-primary">{{$category->name}}</ul>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="row justify-content-center">
+                    <div class="col">
+                        <a href="{{ route('admin.dishes.index', ['dishes' => $dishes]) }}" class="btn btn-primary">Menu</a>
+                    </div>
+                    <div class="col">
+                        <a href="{{ route('admin.orders.index', ['orders' => $orders]) }}" class="btn btn-warning">Ordini</a>
+                    </div>
+            </div>
+        </div>
+</div>
 
-        <td>
-            <a href="{{ route('admin.dishes.index', ['dishes' => $dishes]) }}" class="btn btn-primary">Menu</a>
-        </td>
-        <td>
-            <a href="{{ route('admin.orders.index', ['orders' => $orders]) }}" class="btn btn-warning">Ordini</a>
-        </td>
 @endsection
 
 <style lang="scss" scoped>
-* {
-    color: red;
-}
+
 </style>
