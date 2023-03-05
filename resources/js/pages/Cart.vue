095a3bc6f8b45277c626726e25c8f850ba813057
aCute5 <costantini95dario@gmail.com>
2023-03-05 19:17:12 +0100

    commit per presentazione

--- resources/js/pages/Cart.vue
+++ resources/js/pages/Cart.vue
@@ -1,113 +1,166 @@
 <template>
     <div>
-      <h1>My cart</h1>
-      <div v-for="(item, index) in itemQuantities" :key="index">
-        <h3>{{ item.name }}</h3>
-        <p>
-          Quantity:
-          <button class="btn btn-primary" @click="decreaseQuantity(item)">-</button>
-          {{ item.quantity }}
-          <button class="btn btn-primary" @click="increaseQuantity(item)">+</button>
-        </p>
-        <p>Price: {{ item.price * item.quantity }}$</p>
-      </div>
-      <p>Total amount: {{ totalAmount }}$</p>
-      <button class="btn btn-primary" @click="emptyCart()">Empty cart</button>
-      <router-link class="btn btn-danger" :to="{ name: 'home' }">BACK</router-link>
-      <router-link class="btn btn-warning" :to="{ name: 'checkout' }">CHECKOUT</router-link>
+        <div class="container-fluid">
+            <div
+                class="top-content d-flex justify-content-between align-items-center"
+            >
+                <h1>Cart</h1>
+                <div class="buttons mt-4">
+                    <router-link class="btn btn-danger" :to="{ name: 'home' }"
+                        >BACK</router-link
+                    >
+                    <router-link
+                        class="checkout btn .blue-background"
+                        :to="{ name: 'checkout' }"
+                        >CHECKOUT</router-link
+                    >
+                    <p class="my-3 fs-2">Total amount: {{ totalAmount }}$</p>
+                </div>
+            </div>
+            <div
+                class="cart-item p-2 border w-25"
+                v-for="(item, index) in itemQuantities"
+                :key="index"
+            >
+                <h3 class="cart-item-item">{{ item.name }}</h3>
+                <p class="cart-item-item cart-item-paragraph">
+                    Quantity:
+                    <button class="btn" @click="decreaseQuantity(item)">
+                        -
+                    </button>
+                    {{ item.quantity }}
+                    <button class="btn" @click="increaseQuantity(item)">
+                        +
+                    </button>
+                </p>
+                <p class="cart-item-item">
+                    Price: {{ item.price * item.quantity }}$
+                </p>
+            </div>
+            <div class="right-content">
+                <button class="btn mt-4" @click="emptyCart()">
+                    Empty cart
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-  export default {
+export default {
     name: "Cart",
     props: {
-      cart: Array,
+        cart: Array,
     },
     data() {
-      return {
-        cartData: this.cart,
-      };
+        return {
+            cartData: this.cart,
+        };
     },
     computed: {
-      itemQuantities() {
-        return this.cartData.reduce((accumulator, item) => {
-          if (accumulator[item.name]) {
-            accumulator[item.name].quantity += 1;
-          } else {
-            accumulator[item.name] = {
-              name: item.name,
-              quantity: 1,
-              price: item.price,
-            };
-          }
-          return accumulator;
-        }, {});
-      },
-      totalAmount() {
-    const amount = Object.values(this.itemQuantities).reduce((accumulator, item) => {
-      return accumulator + item.price * item.quantity;
-    }, 0);
-    this.$emit('total-amount', amount);
-    return amount;
-  },
+        itemQuantities() {
+            return this.cartData.reduce((accumulator, item) => {
+                if (accumulator[item.name]) {
+                    accumulator[item.name].quantity += 1;
+                } else {
+                    accumulator[item.name] = {
+                        name: item.name,
+                        quantity: 1,
+                        price: item.price,
+                    };
+                }
+                return accumulator;
+            }, {});
+        },
+        totalAmount() {
+            const amount = Object.values(this.itemQuantities).reduce(
+                (accumulator, item) => {
+                    return accumulator + item.price * item.quantity;
+                },
+                0
+            );
+            this.$emit("total-amount", amount);
+            return amount;
+        },
     },
     methods: {
-      emptyCart() {
-        this.cartData = [];
-        localStorage.setItem("cart", JSON.stringify(this.cartData));
-      },
-      loadCart() {
-        const cart = JSON.parse(localStorage.getItem("cart") || "[]");
-        this.cartData.splice(0, this.cartData.length, ...cart);
-      },
-      increaseQuantity(item) {
-        this.$forceUpdate();
-        this.cart.push({
-          id: item.id,
-          name: item.name,
-          price: item.price,
-          quantity: 1,
-        })
-        item.quantity++;
-        this.saveCart();
-      },
-      decreaseQuantity(item) {
-  if (item.quantity > 1) {
-    this.cart.pop({
-          id: item.id,
-          name: item.name,
-          price: item.price,
-          quantity: 1,
-        })
-    item.quantity--;
-    this.saveCart();
-  } else {
-    const index = this.cart.findIndex(cartItem => cartItem.id === item.id);
-    if (index !== -1) {
-      this.cart.splice(index, 1);
-      this.saveCart();
-    }
-  }
-},
-      saveCart() {
-        localStorage.setItem("cart", JSON.stringify(this.cartData));
-      },
+        emptyCart() {
+            this.cartData = [];
+            localStorage.setItem("cart", JSON.stringify(this.cartData));
+        },
+        loadCart() {
+            const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+            this.cartData.splice(0, this.cartData.length, ...cart);
+        },
+        increaseQuantity(item) {
+            this.$forceUpdate();
+            this.cart.push({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                quantity: 1,
+            });
+            item.quantity++;
+            this.saveCart();
+        },
+        decreaseQuantity(item) {
+            if (item.quantity > 1) {
+                this.cart.pop({
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    quantity: 1,
+                });
+                item.quantity--;
+                this.saveCart();
+            } else {
+                const index = this.cart.findIndex(
+                    (cartItem) => cartItem.id === item.id
+                );
+                if (index !== -1) {
+                    this.cart.splice(index, 1);
+                    this.saveCart();
+                }
+            }
+        },
+        saveCart() {
+            localStorage.setItem("cart", JSON.stringify(this.cartData));
+        },
     },
     mounted() {
-      this.loadCart();
+        this.loadCart();
     },
     watch: {
-      cartData: {
-        handler(newVal) {
-          this.$emit("update:cart", newVal);
-          localStorage.setItem("cart", JSON.stringify(newVal));
+        cartData: {
+            handler(newVal) {
+                this.$emit("update:cart", newVal);
+                localStorage.setItem("cart", JSON.stringify(newVal));
+            },
+            deep: true,
         },
-        deep: true,
-      },
     },
-
 };
 </script>
 
-  <style scoped lang="scss"></style>
+<style scoped lang="scss">
+body {
+    overflow: hidden;
+    font-variant: small-caps !important;
+}
+.cart-item-item {
+    margin-bottom: 0;
+}
+.cart-item-paragraph {
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+}
+button {
+    background-color: black !important;
+    color: white !important;
+    border-radius: none !important;
+}
+.blue-background {
+    background-color: blue !important;
+    color: white;
+}
+</style>
--- resources/js/pages/Checkout.vue
+++ resources/js/pages/Checkout.vue
@@ -1,32 +1,200 @@
 <template>
     <div>
-        <h1>checkout</h1>
-        <v-braintree
-            :authorization="authorization"
-            @success="onSuccess"
-            @error="onError"
-        >
-        </v-braintree>
+        <form @submit.prevent="salvaDatiForm">
+            <div>
+                <label for="f_name">Nome:</label>
+                <input
+                    type="text"
+                    id="f_name"
+                    name="f_name"
+                    v-model="form.f_name"
+                    required
+                />
+            </div>
+            <div>
+                <label for="l_name">Cognome:</label>
+                <input
+                    type="text"
+                    id="l_name"
+                    name="l_name"
+                    v-model="form.l_name"
+                    required
+                />
+            </div>
+            <div>
+                <label for="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    v-model="form.email"
+                    required
+                />
+            </div>
+            <div>
+                <label for="phone_number">Numero di telefono:</label>
+                <input
+                    type="tel"
+                    id="phone_number"
+                    name="phone_number"
+                    v-model="form.phone_number"
+                    required
+                />
+            </div>
+            <div>
+                <label for="address">Indirizzo:</label>
+                <textarea
+                    id="address"
+                    name="address"
+                    v-model="form.address"
+                    required
+                ></textarea>
+            </div>
+            <div>
+                <label for="amount">Importo:</label>
+                <input
+                    value=""
+                    type="number"
+                    id="amount"
+                    name="amount"
+                    v-model="form.amount"
+                    readonly
+                    required
+                />
+            </div>
+            <div>
+                <input
+                    type="hidden"
+                    id="order_date"
+                    name="order_date"
+                    v-model="form.order_date"
+                />
+            </div>
+            <div>
+                <input
+                    type="hidden"
+                    id="pickup_date"
+                    name="pickup_date"
+                    v-model="form.pickup_date"
+                />
+            </div>
+            <div>
+                <input
+                    type="hidden"
+                    id="payment_date"
+                    name="payment_date"
+                    v-model="form.payment_date"
+                />
+                <!-- <input type="hidden" name="dishes[]" value="{{ $dish->id }}"> -->
+            </div>
+            <button type="submit">Invia</button>
+        </form>
     </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
+    name: "Checkout",
     props: {
-    authorization: {
-      required: true,
-      type: String
-    }
-  },
-  methods: {
-    onSuccess (payload) {
-      let nonce = payload.nonce;
-      // Do something great with the nonce...
+        cart: Array,
+        totalAmount: Number,
     },
-    onError (error) {
-      let message = error.message;
-      // Whoops, an error has occured while trying to get the nonce
-    }
-  }
-}
+    data() {
+        return {
+            form: {
+                f_name: "",
+                l_name: "",
+                email: "",
+                phone_number: "",
+                address: "",
+                amount: 0,
+                order_date: "",
+                pickup_date: "",
+                payment_date: "",
+            },
+        };
+    },
+    methods: {
+        salvaDatiForm() {
+            this.form.amount = this.totalAmount;
+            this.form.order_date = this.ottieniData();
+            this.form.pickup_date = this.ottieniData();
+            this.form.payment_date = this.ottieniData();
+            axios
+                .post("/makeOrder", this.form)
+                .then((response) => {
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    console.log(error.response.data);
+                });
+        },
+        ottieniData() {
+            const today = new Date();
+            const yyyy = today.getFullYear();
+            let mm = today.getMonth() + 1;
+            let dd = today.getDate();
+            if (dd < 10) {
+                dd = "0" + dd;
+            }
+            if (mm < 10) {
+                mm = "0" + mm;
+            }
+            return yyyy + "-" + mm + "-" + dd;
+        },
+    },
+};
 </script>
+
+<style lang="scss" scoped>
+form {
+    max-width: 500px;
+    margin: 0 auto;
+    font-size: 16px;
+    font-family: Arial, sans-serif;
+}
+label {
+    display: block;
+    margin-bottom: 5px;
+}
+input[type="text"],
+input[type="email"],
+input[type="tel"],
+textarea {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 2px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
+    font-size: 16px;
+}
+input[type="text"]:focus,
+input[type="email"]:focus,
+input[type="tel"]:focus,
+textarea:focus {
+    border-color: #007bff;
+    outline: none;
+}
+input[type="text"]:read-only,
+input[type="email"]:read-only,
+input[type="tel"]:read-only {
+    background-color: #f5f5f5;
+}
+input[type="submit"] {
+    display: block;
+    margin: 20px auto 0;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #007bff;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+}
+input[type="submit"]:hover {
+    background-color: #0062cc;
+}
+</style>
